982d924042e66ee143885def4aa3bb825de51ad8
aditya03101999 <aa9472042@gmail.com>
2023-04-17 12:32:58 +0530

    This is my third commit

---
+++ src/components/data_preprocessing.py
@@ -0,0 +1,23 @@
+import sys
+import pandas as pd
+from src.logger import logging
+from src.exception import CustomException
+from src.logger import logging
+from sklearn.preprocessing import LabelEncoder
+
+class DataPreprocessing:
+    def __init__(self, data_df, label_encoder):
+        self.data_df = data_df
+        self.label_encoder = label_encoder
+        self.logger = logging
+
+    def encode_labels(self,data):
+        try:
+            # Encode the labels using label encoder
+            label_encoder = LabelEncoder()
+            data['category'] = label_encoder.fit_transform(data['category'])
+            return data, label_encoder
+
+        except Exception as e:
+            self.logger.info(e)
+            raise CustomException(e, sys)
